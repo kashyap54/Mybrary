832e82fd095ef50e26a1ec91012d9c478f74adee
Kashyap Pathiki <kashyappathiki@gmail.com>
2024-02-24 22:39:04 -0500

    Completed APIs to View Edit and Delete Authors

--- models/author.js
+++ models/author.js
@@ -1,9 +1,37 @@
 const mongoose = require("mongoose")
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    }
+})
+
+// authorSchema.pre('deleteOne', function(next) {
+//     Book.find({author: this.id }, (err, books) => {
+//         if (err) {
+//             next(err)
+//         } else if(books.length > 0) {
+//             next(new Error('This author has books still'))
+//         } else {
+//             next()
+//         }
+//     })
+// })
+
+authorSchema.pre("deleteOne", async function (next) {
+    try {
+        const query = this.getFilter();
+        const hasBook = await Book.exists({ author: query._id })
+  
+        if (hasBook) {
+            next(new Error("This author still has books."))
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
     }
 })
 
